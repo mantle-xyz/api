4357580d76020424ed408313ae5de922b8d71d8f
Lot Zhu <lot.zhu@mantle.xyz>
2024-01-25 09:38:33 +0800

    docs: fix picked fields lost

--- types/treasury-token.ts
+++ types/treasury-token.ts
@@ -1,22 +1,25 @@
-export type TreasuryTokenBalance = {
+type TreasuryTokenCommon = {
+  symbol: string;
+  logo_url?: string;
+  price: number;
+  amount: number;
+};
+
+export type TreasuryTokenBalance = TreasuryTokenCommon & {
   id: string;
   chain: string;
   name?: string;
-  symbol: string;
   display_symbol?: string;
   optimized_symbol?: string;
   decimals?: number;
-  logo_url?: string;
   protocol_id?: string;
-  price: number;
   is_core: boolean;
   is_wallet: boolean;
   time_at: number;
-  amount: number;
   raw_amount: number;
 };
 
-export type StatisticToken = Pick<TreasuryTokenBalance, 'symbol' | 'amount' | 'price' | 'logo_url'> & {value: number; name: string; percent: string};
+export type StatisticToken = TreasuryTokenCommon & {value: number; name: string; percent: string};
 
 export type TreasuryStatistic = {
   total: number;
