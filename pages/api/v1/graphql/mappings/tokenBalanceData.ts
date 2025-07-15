5477ced8fa712d00022cb28115db9eb6b60be8d6
Lot Zhu <lot.zhu@mantle.xyz>
2025-07-15 18:02:11 +0800

    fix: type error

--- pages/api/v1/graphql/mappings/tokenBalanceData.ts
+++ pages/api/v1/graphql/mappings/tokenBalanceData.ts
@@ -1,9 +1,6 @@
 // Bind ID against the TOKEN_CONTRACT_ADDRESS
 import { TOKEN_CONTRACT_ADDRESS } from "@/config/general";
 
-// Alchemy has already typed the token balances
-import { TokenBalancesResponse } from "alchemy-sdk";
-
 // Map data exposed in this endpoint to graphql entities
 import { dataHandler as TokenBalance } from "../../token-data";
 
@@ -13,39 +10,6 @@ type Holders = {
   name: string;
   token: string;
   tokenBalance: string | null;
-};
-
-// map TokenBalance entries into the holders entity
-const updateHoldersEntry = (
-  holders: Holders[],
-  name: string,
-  balance_: TokenBalancesResponse
-) => {
-  // check for BitToken
-  const balanceEntry = holders.findIndex((e) => e.id == balance_.address);
-
-  // construct new value set
-  const address = balance_.address;
-  const balance = balance_.tokenBalances.map((balance) => {
-    return {
-      contractAddress: balance.contractAddress,
-      tokenBalance: balance.tokenBalance,
-    };
-  })[0];
-  const tokenBalanceBalance = {
-    id: `${balance.contractAddress}-${address}-${name}`,
-    name: name,
-    token: balance.contractAddress,
-    tokenBalance: balance.tokenBalance,
-  };
-
-  // insert or update entry
-  if (balanceEntry === -1) {
-    // push the new TokenBalanceBalance data
-    holders.push(tokenBalanceBalance);
-  } else {
-    holders.splice(balanceEntry, 1, tokenBalanceBalance);
-  }
 };
 
 // map TokenBalance entries into the holder entities and record TokenBalance entry for summary
@@ -63,38 +27,18 @@ export const mapTokenBalanceData = async (tokens: { address: string }[]) => {
     const tokenBalances = [
       {
         id: TOKEN_CONTRACT_ADDRESS,
-        name: "MNT",
+        name: 'MNT',
         token: TOKEN_CONTRACT_ADDRESS,
         address: TOKEN_CONTRACT_ADDRESS,
         totalSupply: _tokenBalances.totalSupply,
         circulatingSupply: _tokenBalances.circulatingSupply,
-        lockedTotal: _tokenBalances.lockedTotal,
-        balanceTotal: _tokenBalances.treasuryBalanceTotal,
-        LPTokenTotal: _tokenBalances.treasuryLPTokenTotal,
-        mantleCoreTotal: _tokenBalances.mantleCoreTotal,
+        lockedTotal: '0',
+        balanceTotal: _tokenBalances.treasuryBalance,
+        LPTokenTotal: '0',
+        mantleCoreTotal: '0',
       },
     ];
 
-    // prepare to map (we'll place each entry against its name + data)
-    [
-      {
-        name: "BalancesData",
-        data: _tokenBalances.treasuryBalanceData,
-      },
-      {
-        name: "mantleCoreData",
-        data: _tokenBalances.mantleCoreData,
-      },
-      {
-        name: "LPTokenBalancesData",
-        data: _tokenBalances.treasuryLPBalanceData,
-      },
-    ].map(({ name, data }) => updateHoldersEntry(holders, name, data));
-
-    // apply the same to the locked balanceData (these all share a name but not an address)
-    _tokenBalances.lockedBalancesData.map((data) =>
-      updateHoldersEntry(holders, `LockedBalanceData-${data.address}`, data)
-    );
     // return the mapped entities
     return {
       tokens,
